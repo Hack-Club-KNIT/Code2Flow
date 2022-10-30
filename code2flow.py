4511e219ab09b3ea53dc9fdc9167a3cf71972b53
Hack Club KNIT <80102365+HC-KNIT@users.noreply.github.com>
2022-10-30 19:07:57 +0530

    Create code2flow.py

---
+++ code2flow.py
@@ -0,0 +1,6 @@
+def main():
+    print("Software is still under development phase.")
+    print("If you want to contribute, please feel free to add it your code :)")
+
+if __name__ == '__main__':
+    main()
